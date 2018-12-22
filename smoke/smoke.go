52b2cca51b148183d339500c2149d9bd5837411f
yangjinxin <yangjinxinkf@wintrueholding.com>
2018-12-22 23:41:43 +0800

    xxx

--- smoke/smoke.go
+++ smoke/smoke.go
@@ -13,19 +13,23 @@ func main() {
 	}
 
 	pin := rpio.Pin(21)
+	p18 := rpio.Pin(18)
 
 	defer rpio.Close()
 
 	pin.Input()
-	//pin.PullDown()
-
+	p18.Mode(rpio.Pwm)
+	p18.Low()
 	fmt.Println("开启烟雾检测...")
 	for {
 		state := pin.Read()
 		switch state {
 		case rpio.High:
 			fmt.Println("没有烟雾！" + string(state))
+			p18.Low()
 		case rpio.Low:
+			p18.Freq(38000 * 4)
+			p18.DutyCycle(1, 400)
 			fmt.Println("检测到烟雾！" + string(state))
 		}
 		time.Sleep(time.Second * 1)
