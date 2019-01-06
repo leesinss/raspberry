1602c0a022df1c7a060b41e034b664a2e1c43a0d
yangjinxin <yangjinxinkf@wintrueholding.com>
2019-01-06 23:03:38 +0800

    xxx

---
+++ smoke/led.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "github.com/stianeikeland/go-rpio"
+)
+
+func main() {
+    err := rpio.Open()
+    if err != nil {
+        fmt.Printf("GPIO 针脚读取错误！")
+    }
+
+    p6 := rpio.Pin(6)
+    p19 := rpio.Pin(19)
+    p13 := rpio.Pin(13)
+
+    defer rpio.Close()
+
+    p6.Input()
+
+    p6.Low();
+    p13.Low();
+    p19.Low();
+}
